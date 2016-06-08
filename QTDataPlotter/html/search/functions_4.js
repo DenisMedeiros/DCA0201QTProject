c011191fc37b196f5ef68467e72882776bed8a3c
Denis Medeiros <dnsricardo@gmail.com>
2016-06-08 14:50:16 -0300

    Projeto enviado para o professor.

--- QTDataPlotter/html/search/functions_4.js
+++ QTDataPlotter/html/search/functions_4.js
@@ -4,7 +4,6 @@ var searchData=
   ['getmaiory',['getMaiorY',['../a00006.html#a083deba6655c6fa4fcf619a92f45a9d1',1,'Grafico']]],
   ['getmenory',['getMenorY',['../a00006.html#a64f9016a3dea890edccd3e3ab09cc162',1,'Grafico']]],
   ['getmensagem',['getMensagem',['../a00005.html#ad441a74bb54b3a058157a2b3a743e52f',1,'ErroConexao']]],
-  ['gettodosdados',['getTodosDados',['../a00003.html#a64d6279e44720f9d1bb9157a15d9a773',1,'ConexaoPlotter']]],
   ['getultimos30dados',['getUltimos30Dados',['../a00003.html#a10f5f10720574cc76999a893d5327271',1,'ConexaoPlotter']]],
   ['grafico',['Grafico',['../a00006.html#a0d476e2e4912e84bbfb3f6d2ad57b0df',1,'Grafico']]]
 ];
